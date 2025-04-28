763eb4b39c5c3e8d942f5e6472424b69ec0a0c71
Arnob <arnobmuhammad@gmail.com>
2025-04-28 15:30:57 +0600

    feat: Add class-validator and global validation pipe for validating incoming client input.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { GraphqlExceptionFilter } from "./common/filters/graphql-exception.filter";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
   app.useGlobalFilters(new GraphqlExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
