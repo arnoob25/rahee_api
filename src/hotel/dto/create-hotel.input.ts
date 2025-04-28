1665653c8fa566b02459e66d4535a50945f92780
Arnob <arnobmuhammad@gmail.com>
2025-04-28 15:31:50 +0600

    feat: Add validation rules for input fields for creating a new hotel.

--- src/hotel/dto/create-hotel.input.ts
+++ src/hotel/dto/create-hotel.input.ts
@@ -1,11 +1,25 @@
 import { InputType, Field, Float, ID } from "@nestjs/graphql";
 import { Types } from "mongoose";
+import {
+  IsString,
+  Length,
+  IsOptional,
+  IsArray,
+  IsMongoId,
+  IsNumber,
+  Min,
+  Max,
+} from "class-validator";
 
 @InputType()
 export class CreateHotelInput {
   @Field({
     nullable: false,
     description: "Name of the hotel. Example: 'Porco's Hotel'",
+  })
+  @IsString()
+  @Length(1, 100, {
+    message: "Hotel name must be between 1 and 100 characters long.",
   })
   name: string;
 
@@ -14,28 +28,48 @@ export class CreateHotelInput {
     description:
       "Description of the hotel. Example: 'A cozy hotel with a beach view, perfect for families.'",
   })
+  @IsString()
+  @Length(1, 500, {
+    message: "Description must be between 1 and 500 characters long.",
+  })
   description: string;
 
   @Field(() => [ID], {
     nullable: false,
     description: "Available types of rooms in a hotel.",
   })
-  room_types: [Types.ObjectId];
+  @IsArray()
+  @IsMongoId({ each: true })
+  room_type_ids: [Types.ObjectId];
 
   @Field({
     nullable: false,
     description:
       "Address of the hotel. Example: '123 Ocean Ave, Beach City, USA'",
   })
+  @IsString()
+  @Length(1, 200, {
+    message: "Address must be between 1 and 200 characters long.",
+  })
   address: string;
 
   @Field(() => Float, {
+    nullable: true,
     description: "Rating of the hotel. 0-5 stars. Example: 4.5",
   })
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(5)
   star_rating?: number;
 
   @Field(() => Float, {
+    nullable: true,
     description: "Average user review score. 0-10. Example: 8.7",
   })
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(10)
   review_score?: number;
 }
