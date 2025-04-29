c69c47080a5f10d02049ddd2b6790f5c97fa5bdf
Arnob <arnobmuhammad@gmail.com>
2025-04-29 12:21:01 +0600

    feat: Create media schema for references to images and videos.

---
+++ src/common/schemas/media.schema.ts
@@ -0,0 +1,52 @@
+import { Field, ID, ObjectType, registerEnumType } from "@nestjs/graphql";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument, Types } from "mongoose";
+
+export enum MediaType {
+  image = "image",
+  video = "video",
+}
+
+registerEnumType(MediaType, {
+  name: "MediaType",
+  description: "Specifies whether the media is an image or a video.",
+});
+
+@ObjectType({
+  description:
+    "A media asset, such as an image or video, associated with content.",
+})
+@Schema({ timestamps: true })
+export class Media {
+  @Field(() => ID, { description: "Unique identifier for the media item." })
+  _id: Types.ObjectId;
+
+  @Prop({ required: true, type: String, enum: Object.values(MediaType) })
+  @Field(() => MediaType, {
+    description: "The type of media, either image or video.",
+  })
+  type: MediaType;
+
+  @Prop({ required: true })
+  @Field({ description: "The URL where the media file is hosted." })
+  url: string;
+
+  @Prop({ default: false })
+  @Field({
+    nullable: true,
+    defaultValue: false,
+    description: "Marks this media as the cover image.",
+  })
+  is_cover?: boolean;
+
+  @Prop({ default: false })
+  @Field({
+    nullable: true,
+    defaultValue: false,
+    description: "Marks this media as featured content.",
+  })
+  is_featured?: boolean;
+}
+
+export type MediaDocument = HydratedDocument<Media>;
+export const MediaSchema = SchemaFactory.createForClass(Media);
